6d156be56440ec174c55ce13841cdd85f2c3eb34
Victor Mazo <viamazoba@unal.edu.co>
2025-01-19 07:32:32 -0500

    build: Configuración inicial de todo el proyecto.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  StatusBar,
+  View,
+} from 'react-native';
+import { CalculatorScreen } from './presentation/screens/CalculatorScreen';
+
+function App() {
+
+  return (
+    <View>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'black'}
+      />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+
+export default App;
---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+
+export const CalculatorScreen = () => {
+
+    return (
+        <View>
+            <Text>Calculator</Text>
+        </View>
+    );
+};
